635255c9bf8ce13bd90a777e0bddb37a51009052
Wiktor <wiki.lichon@gmail.com>
2023-11-11 20:11:24 +0100

    models and controllers reworked

---
+++ PaperlessAPI/out/src/NPaperless.Services/Controllers/TasksController.cs
@@ -0,0 +1,48 @@
+using FizzWare.NBuilder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mock_Server.Models;
+using System;
+
+namespace Mock_Server.Controllers;
+
+[ApiController]
+[Route("/api")]
+public class TasksController : ControllerBase
+{
+    private readonly ILogger<TasksController> _logger;
+
+    public TasksController(ILogger<TasksController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("tasks")]
+    public IActionResult GetTasks()
+    {
+        Random rand = new Random();
+        int count = 10;
+
+        return Ok(Builder<Models.Task>.CreateListOfSize(count)
+                        .All()
+                        .With(p => p.Type = Models.TaskType.file)
+                        .With(p => p.DateCreated = DateTimeOffset.Now.AddDays(rand.Next(-10, 0)))
+                        .With(p => p.DateDone = DateTimeOffset.Now.AddDays(rand.Next(-10, 0)))
+                        .Build());
+    }
+
+    [HttpOptions("tasks")]
+    public IActionResult Options()
+    {
+        // Return a 200 OK response for CORS preflight requests
+        return Ok();
+    }
+
+    [HttpPost("acknowledge_tasks")]
+    public IActionResult AckTasks([FromBody] AckTasksRequest tasks)
+    {
+        _logger.LogInformation($"Acknowledge tasks: {string.Join(", ", tasks.Tasks)}");
+        return Ok();
+    }
+}
+
---
+++ PaperlessAPI/out/src/NPaperless.Services/Models/AckTasksRequest.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Mock_Server.Models;
+
+public class AckTasksRequest
+{
+    [JsonPropertyName("tasks")]
+    public IEnumerable<int> Tasks { get; set; }
+}
