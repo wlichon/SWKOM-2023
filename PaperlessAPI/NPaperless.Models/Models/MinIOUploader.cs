ddd295af64f5b86ad488c24b6bd20b2d2be89cd7
Wiktor <wiki.lichon@gmail.com>
2023-11-20 04:11:34 +0100

    project restructured

---
+++ PaperlessAPI/NPaperless.Models/Models/MinIOUploader.cs
@@ -0,0 +1,63 @@
+﻿using Minio;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Minio.DataModel.Args;
+using Minio.DataModel;
+using Microsoft.Extensions.Configuration;
+
+public class MinIOUploader
+{
+    private readonly IMinioClient _minioClient;
+
+    private readonly IConfiguration _config;
+
+    private string endpoint = "localhost:9000";
+    private string accessKey = "adkBQ7HSGAS34lJozZyj";
+    private string secretKey = "DU9xjAdodw409joJUnw6A5tbjBsEeU8mbIfXgDWK";
+
+    private string bucketName = "paperless";
+    private string objectName = "paperlessObject";
+
+    public MinIOUploader(IConfiguration config)
+    {
+       
+        
+
+        _minioClient = new MinioClient()
+            .WithEndpoint(endpoint)
+            .WithCredentials(accessKey, secretKey)
+            .Build();
+                        
+    }
+
+    public async Task UploadFileAsync(IFormFile file)
+    {
+
+        // Upload the file to MinIO
+        using (var stream = file.OpenReadStream())
+        {
+            PutObjectArgs poa = new PutObjectArgs()
+                .WithBucket(bucketName)
+                .WithObject(objectName)
+                .WithStreamData(stream)
+                .WithObjectSize(stream.Length)
+                .WithContentType(file.ContentType);
+            try
+            {
+                await _minioClient.PutObjectAsync(poa);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            StatObjectArgs statOjbectArgs = new StatObjectArgs()
+                .WithBucket(bucketName)
+                .WithObject(objectName);
+
+            ObjectStat objectStat = await _minioClient.StatObjectAsync(statOjbectArgs);
+            Console.WriteLine(objectStat.ObjectName, objectStat.ETag);
+        }
+    }
+}
--- PaperlessAPI/openapi-gen.ps1
+++
@@ -1,5 +0,0 @@
-  docker run --name openapi-gen -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/swagger.json -g aspnetcore -p aspnetCoreVersion=6.0 -p pocoModels=true -p useSeperateModelProject=true --package-name NPaperless.Services --model-package NPaperless.Services.DTOs -o /tmp/out/
-  
-  docker cp openapi-gen:/tmp/out/ ./out/
-
-  docker rm -f openapi-gen
